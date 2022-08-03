6f3e80e4f4301d9a970f92f9b3257e35e966c148
object-Object <object@objectobject.ca>
2022-08-03 16:28:08 -0400

    Make file globs work on Windows

--- buildpatterns.py
+++ buildpatterns.py
@@ -1,6 +1,7 @@
 import re
 import json
 import sys
+import glob
 
 # working as of https://github.com/gamma-delta/HexMod/blob/c00815b7b9d90593dc33e3a7539ce87c2ece4fc9/Common/src/main/java/at/petrak/hexcasting/common/casting/RegisterPatterns.java
 # - go to https://github.com/gamma-delta/HexMod/blob/main/Common/src/main/java/at/petrak/hexcasting/common/casting/RegisterPatterns.java
@@ -11,10 +12,11 @@ registry = re.compile(r"PatternRegistry\s*\.\s*mapPattern\s*\(\s*HexPattern\s*\.
 
 # parse the pattern definitions
 pattern_lookup = {}
-for filename in sys.argv[1:]:
-    with open(filename, "r", encoding="utf-8") as file:
-        for match in registry.finditer(file.read()):
-            (pattern, name) = match.groups()
-            pattern_lookup[pattern] = name
+for path in sys.argv[1:]:
+    for filename in glob.glob(path):
+        with open(filename, "r", encoding="utf-8") as file:
+            for match in registry.finditer(file.read()):
+                (pattern, name) = match.groups()
+                pattern_lookup[pattern] = name
 
 json.dump(pattern_lookup, sys.stdout)
